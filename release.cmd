dd73948fb1ed4464517f839586309a74080c23b2
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-01-16 22:59:05 -0400

    Update release.cmd

--- release.cmd
+++ release.cmd
@@ -1,9 +1,9 @@
 @echo off
-set VER=0.1
+set VER=1.0.0
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
-sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/fb-chat-deleter-.+?\.xpi/download\/%VER%\/fb-chat-deleter-%VER%\.xpi/" update.xml
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/neo-hackbar-.+?\.xpi/download\/%VER%\/neo-hackbar-%VER%\.xpi/" update.xml
 
-set XPI=fb-chat-deleter-%VER%.xpi
+set XPI=neo-hackbar-%VER%.xpi
 if exist %XPI% del %XPI%
 zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
